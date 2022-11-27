d000a0ed37e0785383e41c5bafa4edc6569235fe
farahkhan678 <441farahkhan@gmail.com>
2022-11-27 10:30:22 +0000

    first commit

---
+++ src/components/RepositoryItem/index.js
@@ -0,0 +1,49 @@
+// Write your code here
+import './index.css'
+
+const RepositoryItem = props => {
+  const {languageCardDetails} = props
+
+  const {
+    avatarUrl,
+    forksCount,
+    issuesCount,
+    name,
+    starsCount,
+  } = languageCardDetails
+
+  return (
+    <li className="language-card-container">
+      <img src={avatarUrl} alt={name} className="avatar-image" />
+      <h1 className="name">{name}</h1>
+      <div className="containers">
+        <div className="small-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png"
+            alt="stars"
+            className="image"
+          />
+          <p className="count">{starsCount} stars</p>
+        </div>
+        <div className="small-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png"
+            alt="forks"
+            className="image"
+          />
+          <p className="count">{forksCount} forks</p>
+        </div>
+        <div className="small-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png"
+            alt="open issues"
+            className="image"
+          />
+          <p className="count">{issuesCount} open issues</p>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default RepositoryItem
